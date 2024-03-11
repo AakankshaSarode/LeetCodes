88667eec36a83757949ab3187c87cba0a4b3e9c2
AakankshaSarode <aakankshasarode1@gmail.com>
2024-03-11 09:08:41 +0530

    number of provinces

---
+++ medium/numProvinces.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int ans=0,n=isConnected.length;
+        boolean visit[]=new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visit[i]){
+                ans++;
+                dfs(i,visit,isConnected);
+            }
+        }
+        return ans;
+    }
+    public void dfs(int node,boolean[] visit,int[][] isConnected){
+        visit[node]=true;
+        for(int i=0;i<isConnected.length;i++){
+            if(!visit[i] && isConnected[node][i]==1) dfs(i,visit,isConnected);
+        }
+    }
+}
